8609ae91b2fa5f82fd9138a7625ebf231904791f
Mdav07 <166957021+Mdav07@users.noreply.github.com>
2025-03-17 23:39:13 -0500

    Adds the ticker

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup as bs
 from operator import itemgetter
 import tkinter as tk
 from tkinter import ttk
+from yahooquery import search
 
+#Makes the GUI
 def gui(x):
-    columns = ["Company","Stock Price", "Percent Gained"]
+    columns = ["Company","Ticker","Stock Price","Percent Gained","Market Cap (In Millions)"]
     root = tk.Tk()
     root.title("Stock Data")
     tree = ttk.Treeview(root,columns=columns,show="headings")
@@ -16,7 +18,7 @@ def gui(x):
         tree.insert("",tk.END,values=row)
     tree.pack(expand=True,fill="both")
     root.mainloop()
-
+#Ensures that the stock is over 25 million market cap
 def checkMarketCap(text):
     if "M" in text:
         noM = text.replace("M","")
@@ -27,21 +29,28 @@ def checkMarketCap(text):
             return "fail"
     else:
         return "fail"
-
+#Ensures that the stock is over 3 dollars
 def price(price):
     if "," in price:
         price = price.replace(",","")
         return(float(price))
     else:
         return(float(price))
+#Gets the ticker for the company
+def getTicker(name):
+    result = search(name)
+    if 'quotes' in result and result['quotes']:
+        return result['quotes'][0]['symbol']
+    return None
 
+#Finds the pages to search through
 pages = []
-for number in range(1,7):
+for number in range(1,8):
     start = 'https://www.centralcharts.com/en/price-list-ranking/'
     end = 'ALL/desc/ts_19-us-nasdaq-stocks--qc_2-daily-change?p='
     link = start + end + str(number)
     pages.append(link)
-
+#Adds the values in from the website
 values = []
 for page in pages:
     web = requests.get(page)
@@ -62,14 +71,17 @@ for page in pages:
                 rowValues[2]= rowValues[2][1:]
                 rowValues[7] = checkMarketCap(rowValues[7])
                 values.append(rowValues)
-
+#Sorts and output list
 sorted(values,key=itemgetter(2))
 final = []
 for x in values:
     temp = []
     temp.append(x[0])
+    temp.append(str(getTicker(x[0])))
     temp.append(x[1])
     temp.append(x[2])
+    temp.append(x[7])
     final.append(temp)
 
+#Makes the final tool
 gui(final)
